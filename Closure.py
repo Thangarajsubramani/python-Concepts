dd9957f6f33b299d39ac1aaaff8a107a6f66d5af
Thangaraj <rare.thangarajbe@gmail.com>
2017-03-10 16:17:36 -0600

    Create Closure.py

---
+++ Closure.py
@@ -0,0 +1,10 @@
+def generate_power_func(n):
+    print "id(n): %X" % id(n)
+    def nth_power(x):
+        return x**n
+    print "id(nth_power): %X" % id(nth_power)
+    return nth_power
+
+a=generate_power_func(5)
+
+print a(2)
